c2f1a31da50e6633955a0017dd962b9a0182cb12
Kim-Yu-Seong <107562291+kimys5278@users.noreply.github.com>
2023-08-07 13:18:09 +0900

    init

---
+++ src/main/java/com/springboot/godwebsite/GodWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.godwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodWebSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/godwebsite/GodWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.godwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
